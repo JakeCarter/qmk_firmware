d29318654a7019979664a7de0b07a0da3891b6bc
Jake Carter <jake@cartermade.com>
2022-01-27 19:12:30 -0800

    Finally getting around to adding my layout to git.
    Changed default animation/colors.
    Added some custom macros for CMD+Tab window navigation.
    Added some custom macros for text selection.

---
+++ keyboards/planck/ez/glow/keymaps/JakeCarter/config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 110
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+
+#define RGB_MATRIX_STARTUP_SPD 60 // Animation Speed
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_NEXUS
+
+#define RGB_MATRIX_STARTUP_HUE 100
+#define RGB_MATRIX_STARTUP_SAT 255
+#define RGB_MATRIX_STARTUP_VAL 200
