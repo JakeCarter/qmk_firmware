ee8733d93ba1a01cc4186d59af4a82cd81253e5a
Jake Carter <jake@cartermade.com>
2022-01-29 10:14:55 -0800

    Code cleanup/refactor
    Removed unused `RGB_SLD`
    Moved text selection helper implementations to a new file

--- keyboards/planck/ez/glow/keymaps/JakeCarter/rules.mk
+++ keyboards/planck/ez/glow/keymaps/JakeCarter/rules.mk
@@ -1,6 +1,9 @@
 SRC += muse.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+
+SRC += text_edit_helpers.c
+
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 AUDIO_ENABLE = no
