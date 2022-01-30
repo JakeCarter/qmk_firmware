8b0dce14e55afbc258b1dc906aa19c810deb0bec
Jake Carter <jake@cartermade.com>
2022-01-29 18:34:00 -0800

    Fixed key override enable
    I had incorrectly added a #define when trying to enable key overrides.
    Switched to the proper `KEY_OVERRIDE_ENABLE = yes` in the rules file.

--- keyboards/planck/ez/glow/keymaps/JakeCarter/rules.mk
+++ keyboards/planck/ez/glow/keymaps/JakeCarter/rules.mk
@@ -3,10 +3,10 @@ SRC += muse.c
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 
 SRC += text_edit_helpers.c
-SRC += process_key_override.c
 
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 AUDIO_ENABLE = no
 MOUSEKEY_ENABLE = no
 DYNAMIC_MACRO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
