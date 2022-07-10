e353f347305ade05587ccf8b8f7168ff207e148a
Jake Carter <jake@cartermade.com>
2022-07-10 11:33:53 -0700

    Swapped Tab/Escape keys. Updated override to KC_GRAVE.

--- keyboards/planck/ez/glow/keymaps/JakeCarter/keymap.c
+++ keyboards/planck/ez/glow/keymaps/JakeCarter/keymap.c
@@ -47,11 +47,9 @@ float song_error[][2] = SONG(ERROR_SOUND);
 })
 
 //
-// Replace KC_ESCAPE with Shift+Tab if GUI is held down, resulting in CMD+Shift+Tab; Previous App in App Switcher
+// Replace KC_ESCAPE with `KC_GRAVE` if GUI is held down, resulting in CMD+`; In App Switch: Previous App, otherwise Next Window
 //
-// Using CMD+Shift+Tab instead of CMD+` because CMD+` only works after App Switcher has been activated.
-// CMD+Shift+Tab can activate the App Switcher and move to the previous app in one key combo.
-const key_override_t key_override_escape = jc_ko_make_basic(MOD_MASK_GUI, KC_ESCAPE, RSFT(KC_TAB));
+const key_override_t key_override_escape = jc_ko_make_basic(MOD_MASK_GUI, KC_ESCAPE, KC_GRAVE);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -61,8 +59,8 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_grid(
-        KC_TAB,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,
-        KC_ESCAPE,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+        KC_ESCAPE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,
+        KC_TAB,          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
         KC_LSHIFT,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       RETURN_SHIFT,
         JC_LEADER,       KC_LCTRL,       KC_LALT,        KC_LGUI,        LOWER,          KC_SPACE,       KC_NO,          RAISE,          KC_UP,          KC_LEFT,        KC_DOWN,        KC_RIGHT
   ),
